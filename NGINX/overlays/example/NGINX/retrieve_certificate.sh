9f8c41eb73ba1d05873d5bd4d43dfb0e6ef19ed8
TJ Zimmerman <tj@mars.sol.milkyway>
2021-04-11 10:11:20 -0700

    Added NGINX

---
+++ NGINX/overlays/example/NGINX/retrieve_certificate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+certbot certonly \
+	-d tjzimmerman.com \
+	-d www.tjzimmerman.com \
+	-d files.tjzimmerman.com \
+	-d login.tjzimmerman.com \
+	-d tjzimmerman.dev \
+	-d www.tjzimmerman.dev \
+	-d minecraft.tjzimmerman.dev
